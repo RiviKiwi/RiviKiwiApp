23bbd02d5ca61b3b871fb3ea04120e8464401c3c
Artemdjdj <artembasuraa@gmail.com>
2026-01-21 18:18:53 +0300

    [fix] views and forms which help to load amount of images

--- rivikiwi/reviews/forms.py
+++ rivikiwi/reviews/forms.py
@@ -1,9 +1,10 @@
 from django import forms
 from reviews.models import Review
-
+from multiupload.fields import MultiFileField
 
 class ReviewForm(forms.ModelForm):
-    images = forms.FileField(widget=forms.ClearableFileInput(attrs={"multiple": True}))
+
+    images = MultiFileField(min_num=0, max_num=10, required=False)
 
     class Meta:
         model = Review
