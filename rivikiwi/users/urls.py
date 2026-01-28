1bcfd1128104ffae520ba20a238443c2ad6e9483
Artemdjdj <artembasuraa@gmail.com>
2026-01-28 15:58:59 +0300

    [feat] system of changing password using PasswordChangeView

--- rivikiwi/users/urls.py
+++ rivikiwi/users/urls.py
@@ -1,5 +1,6 @@
 
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
 from . import views
 
 app_name = 'users'
@@ -8,5 +9,8 @@ urlpatterns = [
     path('login/', views.login, name="login"),
     path('registration/', views.registration, name="registration"),
     # path('profile/', views.profile, name="profile"),
-    path('logout/', views.logout, name="logout")
+    path('logout/', views.logout, name="logout"),
+    path('password_change/', PasswordChangeView.as_view(template_name="users/password_change.html",
+                                                        success_url=reverse_lazy("users:password_change_done")), name='password_change'),
+    path('password_change/done/', PasswordChangeDoneView.as_view(template_name="users/password_change_done.html"), name='password_change_done'),
 ]
