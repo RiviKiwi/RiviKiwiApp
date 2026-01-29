753e925e4e432a0bbf2ea4fb2c3d6f5a722ca967
Artemdjdj <artembasuraa@gmail.com>
2026-01-29 08:58:49 +0300

    [feat] correct system of changing password

--- rivikiwi/static/assets/js/main-authorization.js
+++ rivikiwi/static/assets/js/main-authorization.js
@@ -4,20 +4,17 @@ passwordFields.forEach(field => {
   const passwordInput = field.querySelector('input');
   const eyeIcon = field.querySelector('.eye img');
   const avatarIcon = document.querySelector('.avatar img');
-  
-  if (passwordInput && eyeIcon && avatarIcon) {
-    // Сохраняем изначальные пути к изображениям
-    const closedEyePath = eyeIcon.src;
-    const openedEyePath = closedEyePath.replace('closed-eye.svg', 'opened-eye.svg');
-    const birdLogPath = avatarIcon.src;
-    const birdOpenPath = birdLogPath.replace('bird-log.svg', 'bird-open-svg.svg');
-
+  if (passwordInput && eyeIcon) {
     eyeIcon.addEventListener('click', () => {
       const isHidden = passwordInput.type === 'password';
       
       passwordInput.type = isHidden ? 'text' : 'password';
-      eyeIcon.src = isHidden ? openedEyePath : closedEyePath;
-      avatarIcon.src = isHidden ? birdOpenPath : birdLogPath;
+      eyeIcon.src = isHidden
+        ? 'assets/images/icons/opened-eye.svg'
+        : 'assets/images/icons/closed-eye.svg';
+      avatarIcon.src = isHidden
+        ? 'assets/images/icons/bird-open-svg.svg'
+        : 'assets/images/icons/bird-log.svg';
     });
   }
 });
