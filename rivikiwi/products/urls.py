26b37d6b4e92c1f3ce00c6e35e63c9e3f241be98
Artemdjdj <artembasuraa@gmail.com>
2026-02-02 16:25:27 +0300

    [feat] index based on class

--- rivikiwi/products/urls.py
+++ rivikiwi/products/urls.py
@@ -5,9 +5,9 @@ from products import views
 app_name = 'products'
 
 urlpatterns = [
-    path('', views.index, name="home"),
-    path('search/', views.index, name="search"),
+    path('', views.IndexView.as_view(), name="home"),
+    path('search/', views.IndexView.as_view(), name="search"),
     path('add_product/', views.add_product, name="add_product"),
-    path('<slug:category_slug>/', views.index, name="index"),
+    path('<slug:category_slug>/', views.IndexView.as_view(), name="index"),
     path('product/<slug:product_slug>/', views.ProductViewController.as_view(), name="product")
 ]
--- rivikiwi/products/views.py
+++ rivikiwi/products/views.py
@@ -3,70 +3,122 @@ from django.shortcuts import render
 from django.core.paginator import Paginator
 from django.urls import reverse
 from .models import Product, ProductCategory, City, ProductImage, ProductView
-from .utils import q_search
+from .utils import q_search, get_client_ip
 from .forms import AddProductForm
 from django.contrib.auth.decorators import login_required
-from django.views.generic.detail import DetailView
+from django.views.generic import DetailView, ListView
 
-def index(request, category_slug=None):
-    max_price = request.GET.get("max_price", None)
-    min_price = request.GET.get("min_price", None)
-    rating = request.GET.get("rating", None)
-    has_discount = request.GET.get("has_discount", None)
-    order_by = request.GET.get("order_by", None)
-    city = request.GET.get("city", None)
-    query = request.GET.get("q", None)
-    page = request.GET.get("page", 1)
-    print(query)
-    
-    # if not category_slug:
-    #     category_slug = "all"
-        
-    category = ProductCategory.objects.get(slug=category_slug) if category_slug else None
-    products = None
-    
-    if category_slug == "all" or (not query and not category_slug): 
-        products = Product.objects.all()
-        category = category = ProductCategory.objects.get(slug="all")
-    elif query:
-        products = q_search(query) 
-    else:
-        products = Product.objects.filter(category__slug=category_slug)
-    
-    if order_by:
-        products = products.order_by(order_by)
-        
-    if city and city != 'Любой город':
-        products = products.filter(city__name=city)
-        
-    if min_price and min_price != '0':
-        products = products.filter(price__gte=int(min_price))
-        
-    if max_price and max_price != '0':
-        products = products.filter(price__lte=int(max_price))
-        
-    if rating:
-        products = products.filter(rating__gte=float(rating))
-        
-    if has_discount:
-        products = products.filter(discount__gt=0)
 
-    paginator = Paginator(products, 3)
-    page = paginator.page(page)
-    
-    context = {
-        'products':page,
-        'category':category,
-    }
-    return render(request, 'products/index.html', context)
 
-def get_client_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-    return ip
+class IndexView(ListView):
+    
+    template_name = "products/index.html"
+    context_object_name = "products"
+    paginate_by = 3
+    
+    def get_queryset(self):
+        category_slug = self.kwargs.get('category_slug', None)
+        
+        max_price = self.request.GET.get("max_price", None)
+        min_price = self.request.GET.get("min_price", None)
+        rating = self.request.GET.get("rating", None)
+        has_discount = self.request.GET.get("has_discount", None)
+        order_by = self.request.GET.get("order_by", None)
+        city = self.request.GET.get("city", None)
+        query = self.request.GET.get("q", None)
+        
+        # if not category_slug:
+        #     category_slug = "all"
+            
+        category = ProductCategory.objects.get(slug=category_slug) if category_slug else None
+        products = None
+        
+        if category_slug == "all" or (not query and not category_slug): 
+            products = Product.objects.all()
+            category = ProductCategory.objects.get(slug="all")
+        elif query:
+            products = q_search(query) 
+        else:
+            products = Product.objects.filter(category__slug=category_slug)
+        
+        if order_by:
+            products = products.order_by(order_by)
+            
+        if city and city != 'Любой город':
+            products = products.filter(city__name=city)
+            
+        if min_price and min_price != '0':
+            products = products.filter(price__gte=int(min_price))
+            
+        if max_price and max_price != '0':
+            products = products.filter(price__lte=int(max_price))
+            
+        if rating:
+            products = products.filter(rating__gte=float(rating))
+            
+        if has_discount:
+            products = products.filter(discount__gt=0)
+        
+        self.category = category
+        
+        return products
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["category"] = self.category
+        return context
+
+# def index(request, category_slug=None):
+#     max_price = request.GET.get("max_price", None)
+#     min_price = request.GET.get("min_price", None)
+#     rating = request.GET.get("rating", None)
+#     has_discount = request.GET.get("has_discount", None)
+#     order_by = request.GET.get("order_by", None)
+#     city = request.GET.get("city", None)
+#     query = request.GET.get("q", None)
+#     page = request.GET.get("page", 1)
+#     print(query)
+    
+#     # if not category_slug:
+#     #     category_slug = "all"
+        
+#     category = ProductCategory.objects.get(slug=category_slug) if category_slug else None
+#     products = None
+    
+#     if category_slug == "all" or (not query and not category_slug): 
+#         products = Product.objects.all()
+#         category = category = ProductCategory.objects.get(slug="all")
+#     elif query:
+#         products = q_search(query) 
+#     else:
+#         products = Product.objects.filter(category__slug=category_slug)
+    
+#     if order_by:
+#         products = products.order_by(order_by)
+        
+#     if city and city != 'Любой город':
+#         products = products.filter(city__name=city)
+        
+#     if min_price and min_price != '0':
+#         products = products.filter(price__gte=int(min_price))
+        
+#     if max_price and max_price != '0':
+#         products = products.filter(price__lte=int(max_price))
+        
+#     if rating:
+#         products = products.filter(rating__gte=float(rating))
+        
+#     if has_discount:
+#         products = products.filter(discount__gt=0)
+
+#     paginator = Paginator(products, 3)
+#     page = paginator.page(page)
+    
+#     context = {
+#         'products':page,
+#         'category':category,
+#     }
+#     return render(request, 'products/index.html', context)
 
 class ProductViewController(DetailView):
     
