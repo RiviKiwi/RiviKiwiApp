6d04d38177c1199e034632e9169c2e07640b208d
Eugene <yevgeniy_kulik@list.ru>
2026-02-05 15:33:47 +0300

    [feat] add wishlist items showing in admin for user

--- rivikiwi/users/admin.py
+++ rivikiwi/users/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+from wishlist.admin import WishlistTabAdmin
 from .models import User
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display=['id', 'username', 'phone', 'email']
-    search_fields=['id', 'username', 'phone', 'email']
+    list_display = ["id", "username", "phone", "email"]
+    search_fields = ["id", "username", "phone", "email"]
+
+    inlines = [WishlistTabAdmin]
