dc4ed2552534dce411d6639dad84c3846c0de0e9
Eugene <yevgeniy_kulik@list.ru>
2026-02-05 15:33:26 +0300

    [feat] add wishlist item model and register it

--- rivikiwi/wishlist/admin.py
+++ rivikiwi/wishlist/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from wishlist.models import WishlistItem
 
-# Register your models here.
+
+class WishlistTabAdmin(admin.TabularInline):
+    model = WishlistItem
+    fields = ("product", "add_date")
+    search_fields = ("product", "add_date")
+    readonly_fields = ("add_date",)
+    extra = 1
+
+
+@admin.register(WishlistItem)
+class WishlistItemAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "product", "add_date"]
+    search_fields = ["id", "user", "add_date"]
+
+    def product_display(self, obj):
+        return str(obj.product)
+
+    def user_display(self, obj):
+        return str(obj.user)
--- rivikiwi/wishlist/models.py
+++ rivikiwi/wishlist/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from rivikiwi import settings
+from products.models import Product
 
-# Create your models here.
+
+class WishlistItem(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Пользователь"
+    )
+    product = models.ForeignKey(
+        to=Product, on_delete=models.CASCADE, verbose_name="Продукт"
+    )
+    add_date = models.DateField(auto_now_add=True, verbose_name="Дата добавления")
+
+    class Meta:
+        db_table = "wishlist"
+        verbose_name = "Список желаемого"
+        verbose_name_plural = "Списки желаемого"
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "product"], name="unique_product_in_wishlist"
+            )
+        ]
